8a52c49a707775aa321a2f5df8ec20819ec1b9b6
vertoker <vertogpro@gmail.com>
2025-03-25 08:46:39 +0500

    add first realization for almost all functions/operators

--- src/String.cpp
+++ src/String.cpp
@@ -27,8 +27,11 @@ String::String(const char* str)
     data = new char[capacity + 1];
     memcpy(data, str, capacity + 1);
 }
+String::String(const size_t newSize, const size_t newCapacity, char* newData)
+    : size{newSize}, capacity{newCapacity}, data(newData) { }
 
 // move/copy ctors/operators
+
 String::String(const String& other) : size{other.size}, capacity{other.capacity}
 {
     data = new char[capacity + 1];
@@ -72,3 +75,127 @@ String& String::operator=(String&& other) noexcept
     return *this;
 }
 
+// operators
+
+bool vertoker::operator==(const String& lhs, const String& rhs)
+{
+    if (lhs.size != rhs.size) return false;
+
+    for (size_t i = 0; i < lhs.size; i++)
+        if (lhs[i] != rhs[i]) return false;
+    return true;
+}
+bool vertoker::operator==(const String& lhs, const char* rhs) // TODO
+{
+    //if (lhs.size != strlen(rhs)) return false;
+    return false;
+}
+bool vertoker::operator!=(const String& lhs, const String& rhs)
+    { return !(lhs == rhs); }
+bool vertoker::operator!=(const String& lhs, const char* rhs)
+    { return !(lhs == rhs); }
+
+void String::operator+=(const String& rhs)
+{
+    size_t newSize = size + rhs.size;
+    if (capacity < newSize) Reserve(newSize);
+    memcpy(data + size, rhs.data, rhs.size + 1);
+}
+void String::operator+=(const char* rhs)
+{
+    size_t rhsSize = strlen(rhs);
+    size_t newSize = size + rhsSize;
+    if (capacity < newSize) Reserve(newSize);
+    memcpy(data + size, rhs, rhsSize + 1);
+}
+
+String vertoker::operator+(const String& lhs, const String& rhs)
+{
+    size_t newSize = lhs.size + rhs.size;
+    char* newData = new char[newSize + 1];
+
+    memcpy(newData, lhs.data, lhs.size);
+    memcpy(newData + lhs.size, rhs.data, rhs.size + 1);
+    
+    return String(newSize, newSize, newData);
+}
+String vertoker::operator+(const String& lhs, const char* rhs)
+{
+    size_t rhsSize = strlen(rhs);
+    size_t newSize = lhs.size + rhsSize;
+    char* newData = new char[newSize + 1];
+
+    memcpy(newData, lhs.data, lhs.size);
+    memcpy(newData + lhs.size, rhs, rhsSize + 1);
+    
+    return String(newSize, newSize, newData);
+}
+
+std::istream& vertoker::operator>>(std::istream& is, String& str)
+{
+    char buffer;
+    is.get(buffer);
+
+    while (buffer != '\n')
+    {
+        str.PushBack(buffer);
+        is.get(buffer);
+    }
+    
+    return is;
+}
+std::ostream& vertoker::operator<<(std::ostream& os, String& str)
+{
+    for (size_t i = 0; i < str.size; ++i)
+        os << str[i];
+    return os;    
+}
+
+// functions
+
+void String::Reserve(const size_t newCapacity)
+{
+    if (newCapacity <= capacity) return;
+
+    char* newData = new char[newCapacity + 1];
+    if (data != nullptr)
+    {
+        memcpy(newData, data, size + 1);
+        delete[] data;
+    }
+
+    capacity = newCapacity;
+    data = newData;
+}
+void String::Resize(const size_t newSize, const char defaultChar) // TODO
+{
+    if (newSize <= size) return;
+
+    if (newSize > capacity)
+    {
+        size_t nextCapacity = GetNextCapacity();
+        Reserve((nextCapacity > newSize) ? nextCapacity : newSize);
+    }
+
+    memset(data + size, defaultChar, newSize - size);
+    size = newSize;
+    data[size - 1] = 0;
+}
+void String::PushBack(char character)
+{
+    if (GetUnusedCapacity() == 0)
+        Reserve(GetNextCapacity());
+    
+    data[size] = character;
+    data[++size] = 0;
+}
+void String::Clear() noexcept
+{
+    memset(data, 0, capacity);
+    size = 0;
+}
+
+size_t String::GetUnusedCapacity() const
+    { return capacity - size; }
+size_t String::GetNextCapacity() const
+    { return capacity == 0 ? 1 : (size_t)(capacity * GrowthFactor); }
--- src/String.h
+++ src/String.h
@@ -17,12 +17,28 @@ namespace vertoker
 
         // other ctors
         String(const char* str);
+        String(const size_t newSize, const size_t newCapacity, char* newData);
 
         // copy/move ctors/operators
         String(const String& other);
         String(String&& other) noexcept;
         String& operator=(const String& other);
         String& operator=(String&& other) noexcept;
+
+        // operators
+        friend bool operator==(const String& lhs, const String& rhs);
+        friend bool operator==(const String& lhs, const char* rhs);
+        friend bool operator!=(const String& lhs, const String& rhs);
+        friend bool operator!=(const String& lhs, const char* rhs);
+
+        void operator+=(const String& rhs);
+        void operator+=(const char* rhs);
+
+        friend String operator+(const String& lhs, const String& rhs);
+        friend String operator+(const String& lhs, const char* rhs);
+
+        friend std::istream& operator>>(std::istream& is, String& str);
+        friend std::ostream& operator<<(std::ostream& os, String& str);
         
         // "fields"
         inline char& operator[](size_t index) const { return data[index]; }
@@ -31,12 +47,35 @@ namespace vertoker
         inline size_t GetCapacity() const noexcept { return capacity; }
         inline char* GetData() const noexcept { return data; }
 
+        // public functions
+        void Reserve(const size_t newCapacity);
+        void Resize(const size_t newSize, const char defaultChar);
+        void PushBack(char character);
+        void Clear() noexcept;
+
     private:
+        // private functions
+        size_t GetUnusedCapacity() const;
+        size_t GetNextCapacity() const;
+        
+        // data
         size_t size;
         size_t capacity;
         char* data;
     };
-    
+
+    // external operators
+    bool operator==(const String& lhs, const String& rhs);
+    bool operator==(const String& lhs, const char* rhs);
+    bool operator!=(const String& lhs, const String& rhs);
+    bool operator!=(const String& lhs, const char* rhs);
+
+    String operator+(const String& lhs, const String& rhs);
+    String operator+(const String& lhs, const char* rhs);
+
+    std::istream& operator>>(std::istream& is, String& str);
+    std::ostream& operator<<(std::ostream& os, String& str);
+
 } // namespace vertoker
 
 
